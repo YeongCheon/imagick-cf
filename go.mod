e2e0c19881f8ccc8626bba0330e18fdd9ef1c8cf
YeongCheon Kim <kyc1682@gmail.com>
2020-12-19 23:02:06 +0900

    implement reading files from cloud storage bucket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yeongcheon/imagick-cf
 
 go 1.15
+
+require cloud.google.com/go/storage v1.12.0
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	bucketName = "BUCKET_NAME"
+)
+
+var (
+	proejctId     = os.Getenv("GOOGLE_CLOUD_PROJECT")
+	storageClient *storage.Client
+	bucket        *storage.BucketHandle
+)
+
+func init() {
+	var err error
+	storageClient, err = storage.NewClient(context.Background())
+
+	if err != nil {
+		log.Fatalf("storage.NewClient: %v", err)
+	}
+
+	bucket = storageClient.Bucket(bucketName)
+}
+
+func ReceiveHttp(w http.ResponseWriter, r *http.Request) {
+	name := strings.Split(r.URL.Path, "/")[0]
+	existFileReader, err := bucket.Object(name).NewReader(context.Background())
+	if err == nil {
+		defer existFileReader.Close()
+		io.Copy(w, existFileReader)
+	} else {
+		log.Fatal(err)
+	}
+}
