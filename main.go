7cda6f9925b5b528c0dbdce2b2eb3eeaa2d12d9e
YeongCheon Kim <kyc1682@gmail.com>
2020-12-20 23:14:23 +0900

    add "imageProcess" function for optimize image file size

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 const (
-	bucketName = "BUCKET_NAME"
+	bucketName          = "BUCKET_NAME"
+	optimizedFilePrefix = "optimize"
 )
 
 var (
@@ -31,14 +33,50 @@ func init() {
 	bucket = storageClient.Bucket(bucketName)
 }
 
+func imageProcess(
+	ctx context.Context,
+	originalImageName,
+	outputImageName string,
+) (*storage.ObjectHandle, error) {
+	originalImage := bucket.Object(originalImageName)
+	r, err := originalImage.NewReader(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	resultImage := bucket.Object(outputImageName)
+	w := resultImage.NewWriter(ctx)
+	defer w.Close()
+
+	// Use - as input and output to use stdin and stdout.
+	cmd := exec.Command("convert", "-strip", "-interlace", "Plane", "-gaussian-blur", "0.05", "-quality", "85%", "-", "-")
+	cmd.Stdin = r
+	cmd.Stdout = w
+
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	return resultImage, nil
+}
+
 func ReceiveHttp(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL.Path)
-	name := strings.TrimPrefix(r.URL.Path, "/")
-	existFileReader, err := bucket.Object(name).NewReader(context.Background())
+	imageName := strings.TrimPrefix(r.URL.Path, "/")
+	optimizedFileName := strings.Join([]string{optimizedFilePrefix, imageName}, "/")
+
+	existFileReader, err := bucket.Object(optimizedFileName).NewReader(context.Background())
 	if err == nil {
 		defer existFileReader.Close()
 		io.Copy(w, existFileReader)
 	} else {
-		log.Fatal(err)
+		result, err := imageProcess(context.Background(), imageName, optimizedFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		resultReader, _ := result.NewReader(context.Background())
+		defer resultReader.Close()
+
+		io.Copy(w, resultReader)
 	}
 }
